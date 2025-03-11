1ff019c511d6dba4640be747556f24cbac803006
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-03-11 11:33:01 +0500

    Update secure_vault.py

--- secure_vault.py
+++ secure_vault.py
@@ -5,3 +5,13 @@ import base64
 import getpass
 import pyperclip
 from cryptography.fernet import Fernet
+
+# Database & Key Setup
+db_file = "vault.db"
+key_file = "key.key"
+
+# Generate and load encryption key
+def generate_key():
+    key = Fernet.generate_key()
+    with open(key_file, "wb") as keyfile:
+        keyfile.write(key)
